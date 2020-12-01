d7c9e1003b2b4d009ed5c1421b18a576007539c2
brugobi <brgobbi@hotmail.com>
2020-12-01 14:21:35 -0500

    add form template

--- index.js
+++ index.js
@@ -1,43 +1,30 @@
-// fetch(`https://jsonplaceholder.typicode.com/users`)
+// to get the KEY
+// let newGame = {
+//   name: "DinosGame"
+// }
+
+// fetch(`https://us-central1-js-capstone-backend.cloudfunctions.net/api/games`, {
+//   method: 'post',
+//   headers: {
+//       'content-type': 'application/json',
+//     },
+//     body: JSON.stringify(newGame)
+//   })
 //   .then(function (response) {
 //     return response.json()
 //   })
 //   .then(function (data) {
-//     console.log('the data', data)
+//     console.log('post request response data', data)
 //   })
 
-// To create the API key 
-// fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/qzwGXOSsvjtPlYTJApXC/scores/', {
-  //   method: 'POST',
-  //   headers: {
-  //     'content-type': 'application/json',
-  //   },
-  //   body: JSON.stringify(gameName)
-  //   })
-  //   .then(function(response) {
-  //     return response.json();
-  //   })
-  //   .then(function(response) {
-  //     console.log(response);
-  //   })
-  //   .catch((err) => {
-  //     console.log(err);
-  //   });
 
-let newGame = {
-  name: "DinosGame"
-}
 
-fetch(`https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/DinosGame`, {
-  method: 'post',
-  headers: {
-      'content-type': 'application/json',
-    },
-    body: JSON.stringify(newGame)
-  })
-  .then(function (response) {
-    return response.json()
-  })
-  .then(function (data) {
-    console.log('post request response data', data)
-  })
+const myForm = document.getElementById('myForm');
+
+myForm.addEventListener('submit', function (e) {
+  e.preventDefault();
+
+  const formData = new FormData(this);
+
+  fetch()
+});
